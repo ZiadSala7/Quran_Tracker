998c81b217755e620c19856c90434262c92301d9
Ziad <zslah2800@gmail.com>
2025-10-15 15:46:16 +0300

    delete flutter

--- lib/islamic_app.dart
+++ lib/islamic_app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/utils/app_routes.dart';
 import 'features/splash/views/splash_view.dart';
@@ -9,20 +8,15 @@ class IslamicApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      child: MaterialApp(
-        locale: Locale('ar'),
-        routes: appRoutes,
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: 'Haramatton',
-          scaffoldBackgroundColor: Colors.white,
-        ),
-        home: SplashView(),
+    return MaterialApp(
+      locale: Locale('ar'),
+      routes: appRoutes,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Haramatton',
+        scaffoldBackgroundColor: Colors.white,
       ),
+      home: SplashView(),
     );
   }
 }
